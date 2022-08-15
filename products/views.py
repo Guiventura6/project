63011b9024dfd4b8f002a40196eb0a7b3864c3ce
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-08-15 15:33:07 -0300

    Create template home and workspace global for files templates global

--- products/views.py
+++ products/views.py
@@ -1,11 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-# from django.shortcuts import render
 
-
-# Create your views here.
 def home(request):
-    return HttpResponse('Home 1')
+    return render(request, 'products/home.html')
 
 
 def about(request):
