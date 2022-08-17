6a86798dda9db7473ef3122d3a4295fdebd3d01b
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-08-17 12:18:12 -0300

    Including font-awesome in template home.

--- products/views.py
+++ products/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'products/home.html')
+    return render(request, 'products/home.html',
+                  context={
+                      'name': 'Name product',
+                  })
